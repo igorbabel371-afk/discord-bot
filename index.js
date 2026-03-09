815dda4f3e2880eb53c6cba83b6ece1939c010b1
igorbabel371-afk <igorbabel371@gmail.com>
2026-03-10 00:44:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+const { Client, GatewayIntentBits, ActionRowBuilder, StringSelectMenuBuilder, EmbedBuilder } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('clientReady', () => {
+  console.log(`Bot działa jako ${client.user.tag}`);
+});
+
+client.on('messageCreate', async message => {
+
+  if (message.content === "!produkty") {
+
+    const embed = new EmbedBuilder()
+      .setTitle("📦 Sklep")
+      .setDescription("Wybierz kategorię produktów");
+
+    const menu = new StringSelectMenuBuilder()
+      .setCustomId('kategorie')
+      .setPlaceholder('Wybierz kategorię')
+      .addOptions([
+        {
+          label: 'Produkty Discord',
+          value: 'discord',
+        },
+        {
+          label: 'Streaming',
+          value: 'streaming',
+        },
+        {
+          label: 'Roblox',
+          value: 'roblox',
+        }
+      ]);
+
+    const row = new ActionRowBuilder().addComponents(menu);
+
+    await message.channel.send({
+      embeds: [embed],
+      components: [row]
+    });
+
+  }
+
+});
+
+client.on('interactionCreate', async interaction => {
+
+  if (!interaction.isStringSelectMenu()) return;
+
+  if (interaction.values[0] === "discord") {
+
+    const embed = new EmbedBuilder()
+      .setTitle("Produkty Discord")
+      .setDescription(`
+• Discord Nitro — **30 zł / 1 miesiąc**  
+• Serwer Boosty — **25 zł / 14 boost / miesiąc**  
+• Discord Members — **18 zł / 500 members**
+`);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+
+  }
+
+  if (interaction.values[0] === "streaming") {
+
+    const embed = new EmbedBuilder()
+      .setTitle("Streaming")
+      .setDescription(`
+• Netflix — **20 zł / 1 miesiąc**  
+• Disney+ — **15 zł / 1 miesiąc**  
+• HBO Max — **15 zł / 1 miesiąc**  
+• Paramount+ — **18 zł / 1 miesiąc**  
+• YouTube Premium — **17 zł / 1 miesiąc**
+`);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+
+  }
+
+  if (interaction.values[0] === "roblox") {
+
+    const embed = new EmbedBuilder()
+      .setTitle("Roblox")
+      .setDescription(`
+• case paradise **1 titan / 7 zł**  
+• case paradise **5 titan / 35 zł**  
+• case paradise **10 titan / 70 zł**  
+• case paradise **15 titan / 100 zł**
+`);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+
+  }
+
+});
+
+client.login(process.env.TOKEN);
